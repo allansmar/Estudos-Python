8343c02d43a5776fa3aa79dbc2db2cbb9a3434a1
allansmar <111984890+allansmar@users.noreply.github.com>
2022-08-24 10:37:02 -0300

    Add files via upload

---
+++ Atividade_01.py
@@ -0,0 +1,6 @@
+# Questão 01 - Faça um programa que converta metros para centímetros.
+
+print("Vamos converter metros para centímetros ")
+valor = float(input("Insira um valor em metros para ser convertido em centímetros: "))
+convert = int((valor * 100))
+print(f'{valor} m em metros é: {convert} cm')
