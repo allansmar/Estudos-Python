a3a1629c546b88d534bc94b15f7b326d8d61fbf8
allansmar <111984890+allansmar@users.noreply.github.com>
2022-08-31 11:56:04 -0300

    Add files via upload

---
+++ While.py
@@ -0,0 +1,12 @@
+while True:
+    nota1 = int(input("Digite uma nota de 0 a 10: "))
+    if 0 <= nota1 or nota1 = > 10:
+        print(f"Você digitou {nota1}. Ok!")
+        continue
+    else:
+        print(f"{nota1} não está dentro do intervalo.")
+        break
+
+
+
+
