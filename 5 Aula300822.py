227bcef35f300720cb04c21e539219f52cfbb34c
allansmar <111984890+allansmar@users.noreply.github.com>
2022-08-31 11:21:17 -0300

    Add files via upload

---
+++ 5 Aula300822.py
@@ -0,0 +1,12 @@
+numero = 1
+
+while numero <= 100:
+    if numero % 2 ==0:
+        print(f" {numero} é par ")
+    numero = numero + 1
+print("----------------------------------------")
+numero = 0
+while numero <= 100:
+    if numero % 3 == 0  :
+        print(f" {numero} é impar ")
+    numero = numero + 1
