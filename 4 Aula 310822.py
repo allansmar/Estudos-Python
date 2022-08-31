85ba69946ef40ec26c663d2f65b5a49eeb8d6b2d
allansmar <111984890+allansmar@users.noreply.github.com>
2022-08-31 10:37:57 -0300

    Add files via upload

---
+++ 4 Aula 310822.py
@@ -0,0 +1,12 @@
+# numero = 0
+
+# while numero <=50:
+#    print(f"{numero}")
+#    numero = numero + 1
+# na linha 5 também pode ser: contador +1 =
+
+numero = 50
+
+while not numero == 9:
+    print(f" {numero}")
+    numero = numero - 1
