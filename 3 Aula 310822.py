2106d5cdafd2e56e77587547824b3edfd0b358a0
allansmar <111984890+allansmar@users.noreply.github.com>
2022-08-31 10:10:10 -0300

    Add files via upload

---
+++ 3 Aula 310822.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um numero inteiro e positivo: "))
+div = float(num/2)
+div2 = float(num/3)
+if num % 2 == 0:
+    print(f"{num} é par! ")
+else:
+    num % 3 == 0
+    print(f"{num} é impar!" )
