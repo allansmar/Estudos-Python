7bdf939f19ebe809f52d716f54e9050006aad60a
allansmar <111984890+allansmar@users.noreply.github.com>
2022-09-21 09:56:11 -0300

    Add files via upload

---
+++ aquecimento2.py
@@ -0,0 +1,13 @@
+'''
+Criar tabuada utilizando while e depois o for
+'''
+
+# While
+
+contador = 1
+num = int(input("Informe o numero: "))
+
+while contador <=10:
+    multiplicacao = num*contador
+    print(f"{numero} x {contador} = {multiplicacao}")
+    contador = contador + 1
