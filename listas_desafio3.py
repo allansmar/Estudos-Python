d096f2df9871e3e401f7e8d2334fa02daf467635
allansmar <111984890+allansmar@users.noreply.github.com>
2022-10-05 11:20:38 -0300

    Add files via upload

--- listas_desafio3.py
+++ listas_desafio3.py
@@ -1,18 +1,23 @@
 lista = []
-contador = 0
 somapar = []
 somaimpar = []
+contador = 0
 
-for contador in range(10):
+for contador in range(6):
     numeros= int(input(f"Adicione o {contador + 1}º número inteiro e positivo: "))
-    if numeros %2 == 0:
-        lista.append(somapar)
-    elif numeros %2 != 0:
-        lista.append(somaimpar)
-    lista.append(numeros)
-    contador = contador + 1
+    if numeros == 0:
+        print("não digite 0")
+    else:
+        while numero !=0:
+            contador = contador + 1
+            lista.append(numeros)
+            if numeros %2 == 0:
+                somapar.append(numeros)
+            else:
+               somaimpar.append(numeros)
 
-print(f"A soma dos pares é: {sum(somapar)}")
-print(f"A soma dos impares é: {sum(somaimpar)}")
-print(f"O menor é: {min(lista)}")
-print(f"A lista: {lista}")
+print(f"A lista original\n{lista}")
+print(f"A lista dos pares: {somapar}\nsoma dos pares: {sum(somapar)}")
+print(f"A lista dos impares: {somaimpar}\nsoma dos impares: {sum(somaimpar)}")
+
+#Precisa validar a condição de ser positivo!
