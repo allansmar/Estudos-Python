0794d3e816c112cfb3dbd9a51d5993ad05a9cfa9
allansmar <111984890+allansmar@users.noreply.github.com>
2022-10-19 12:29:03 -0300

    Add files via upload

---
+++ dicionarios.py
@@ -0,0 +1,13 @@
+dados = {}
+
+nome = input("Informe seu NOME: ")
+idade = input("Informe sua IDADE: ")
+cpf = input("Informe o CPF: ")
+endereco = input("Informe ENDEREÇO: ")
+
+dados["Nome"] = nome
+dados["Idade"] = idade
+dados["Cpf"] = cpf
+dados["endereco"] = endereco
+
+print(dados)
