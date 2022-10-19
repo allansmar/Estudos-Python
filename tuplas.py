55d6f04a2b7ad650c3f8807f6eda22097919025e
allansmar <111984890+allansmar@users.noreply.github.com>
2022-10-19 12:28:35 -0300

    Add files via upload

---
+++ tuplas.py
@@ -0,0 +1,13 @@
+# ATIVIDADE 1
+'''lat = (-82.1556)
+long = (129.7098)
+print(f'Cidade: Tangamandapio\nlatitude: {lat}\nlongitude: {long}')'''
+
+# ATIVIDADE 2
+
+tupla =("Tangamandapio","-82.1556","129.7098")
+lista = list(tupla)
+lista[0] = "Olinda"
+lista[1] = "latitude"
+lista[2] = "longitude"
+
