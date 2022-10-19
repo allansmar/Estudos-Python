975df3dade0dc6ce9a78481c6c0b4f38eaa96089
allansmar <111984890+allansmar@users.noreply.github.com>
2022-10-19 12:28:14 -0300

    Add files via upload

---
+++ Sets_Tuplas_Dicionarios.py
@@ -0,0 +1,16 @@
+x = {1,2,3,4,5,6}
+y = {5,6,7,8,9}
+
+
+print(type(x))
+
+# União
+print("União: ")
+print(x | y)
+
+# Intersecção
+print("Intersecção: ")
+print(x & y)
+
+print("Simetrico: ")
+print(x ^ y)
